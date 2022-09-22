c0cc08e737e1f0fdbc45d492b825ffeda43dfe50
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-22 20:30:33 +0330

    add layout wrapper

---
+++ layout/index.js
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+  return (
+    <>
+      <div>Hello From Layout!</div>
+      {children}
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import Layout from "../layout";
 import "../style/reset.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
