42050c7c4a9a0f3b1a807ffd295dae8bb72d9d2e
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-23 15:46:30 +0330

    add grid for detail page

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,10 +1,20 @@
-import { Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import KeyboardBackspaceRoundedIcon from "@mui/icons-material/KeyboardBackspaceRounded";
 
 export default function Country() {
   return (
-    <Button variant="contained" startIcon={<KeyboardBackspaceRoundedIcon />}>
-      Back
-    </Button>
+    <>
+      <Button variant="contained" startIcon={<KeyboardBackspaceRoundedIcon />}>
+        Back
+      </Button>
+      <Grid container spacing={2} mt={5}>
+        <Grid item xs={12} md={6}>
+          asdfasdfasdf
+        </Grid>
+        <Grid item xs={12} md={6}>
+          asdfasdf
+        </Grid>
+      </Grid>
+    </>
   );
 }
