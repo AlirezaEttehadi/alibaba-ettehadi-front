5d46cd9c05a76c8b396378534ab4e25b6ac2e4e6
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-23 15:05:22 +0330

    add sample card

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,15 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import { Grid, useMediaQuery } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 
 export default function Home() {
   const theme = useTheme();
@@ -20,64 +28,88 @@ export default function Home() {
     seetRegion(event.target.value);
   };
   return (
-    <Grid
-      container
-      spacing={2}
-      direction="row"
-      justifyContent="space-between"
-      alignItems="center"
-    >
-      <Grid item xs={12} md={4}>
-        <Paper
-          component="form"
-          sx={{
-            p: "2px 4px",
-            display: "flex",
-            alignItems: "center",
-            backgroundColor: theme.palette.primary.main,
-          }}
-        >
-          <IconButton type="button" sx={{ p: "10px" }}>
-            <SearchIcon />
-          </IconButton>
-          <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="Search for a country..."
-          />
-        </Paper>
-      </Grid>
+    <>
       <Grid
-        item
-        xs={12}
-        md={8}
-        display="flex"
-        justifyContent={isDesktop ? "flex-end" : "flex-start"}
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        mb="36px"
       >
-        <FormControl
-          sx={{
-            backgroundColor: theme.palette.primary.main,
-            minWidth: 200,
-          }}
-        >
-          <InputLabel id="filter-by-region-label">Filter by Region</InputLabel>
-          <Select
-            labelId="filter-by-region-label"
-            id="filter-by-region"
-            value={region}
-            label="Filter by Region"
-            onChange={handleChange}
+        <Grid item xs={12} md={4}>
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              backgroundColor: theme.palette.primary.main,
+            }}
           >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value="Africa">Africa</MenuItem>
-            <MenuItem value="Americas">Americas</MenuItem>
-            <MenuItem value="Asia">Asia</MenuItem>
-            <MenuItem value="Europe">Europe</MenuItem>
-            <MenuItem value="Oceania">Oceania</MenuItem>
-          </Select>
-        </FormControl>
+            <IconButton type="button" sx={{ p: "10px" }}>
+              <SearchIcon />
+            </IconButton>
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search for a country..."
+            />
+          </Paper>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          display="flex"
+          justifyContent={isDesktop ? "flex-end" : "flex-start"}
+        >
+          <FormControl
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              minWidth: 200,
+            }}
+          >
+            <InputLabel id="filter-by-region-label">
+              Filter by Region
+            </InputLabel>
+            <Select
+              labelId="filter-by-region-label"
+              id="filter-by-region"
+              value={region}
+              label="Filter by Region"
+              onChange={handleChange}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value="Africa">Africa</MenuItem>
+              <MenuItem value="Americas">Americas</MenuItem>
+              <MenuItem value="Asia">Asia</MenuItem>
+              <MenuItem value="Europe">Europe</MenuItem>
+              <MenuItem value="Oceania">Oceania</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
       </Grid>
-    </Grid>
+      <Card sx={{ maxWidth: 345, backgroundColor: theme.palette.primary.main }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </>
   );
 }
