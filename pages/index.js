5ee8538ec7384d7760e2dcb4935812556d737904
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-23 21:42:34 +0330

    add link for every country

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Link from "next/link";
 
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
@@ -98,43 +99,49 @@ export default function Home({ countries }) {
       <Grid container spacing={2}>
         {countries?.map((country) => {
           return (
-            <Grid key={country.name} item xs={12} sm={6} md={4} lg={3}>
-              <Card sx={{ backgroundColor: theme.palette.primary.main }}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={country.flags.svg}
-                    alt={`The falg of${country.name}`}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {country.name}
-                    </Typography>
-                    <Box display="flex" alignItems="center">
-                      <Typography variant="body1" mr={0.5}>
-                        Population:
+            <Link key={country.name} href={`/${country.name}`}>
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Card sx={{ backgroundColor: theme.palette.primary.main }}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={country.flags.svg}
+                      alt={`The falg of${country.name}`}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        {country.name}
                       </Typography>
-                      <Typography variant="body2">
-                        {country.population}
-                      </Typography>
-                    </Box>
-                    <Box display="flex" alignItems="center">
-                      <Typography variant="body1" mr={0.5}>
-                        Region:
-                      </Typography>
-                      <Typography variant="body2">{country.region}</Typography>
-                    </Box>
-                    <Box display="flex" alignItems="center">
-                      <Typography variant="body1" mr={0.5}>
-                        Capital:
-                      </Typography>
-                      <Typography variant="body2">{country.capital}</Typography>
-                    </Box>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
+                      <Box display="flex" alignItems="center">
+                        <Typography variant="body1" mr={0.5}>
+                          Population:
+                        </Typography>
+                        <Typography variant="body2">
+                          {country.population}
+                        </Typography>
+                      </Box>
+                      <Box display="flex" alignItems="center">
+                        <Typography variant="body1" mr={0.5}>
+                          Region:
+                        </Typography>
+                        <Typography variant="body2">
+                          {country.region}
+                        </Typography>
+                      </Box>
+                      <Box display="flex" alignItems="center">
+                        <Typography variant="body1" mr={0.5}>
+                          Capital:
+                        </Typography>
+                        <Typography variant="body2">
+                          {country.capital}
+                        </Typography>
+                      </Box>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            </Link>
           );
         })}
       </Grid>
